3ddc84736c8b15714e618d7bd4730498b27bd591
xfrnk2 <xfrnk2@gmail.com>
2022-04-17 01:59:15 +0900

    feat: 2022-04-17

---
+++ 60-LeetCode-Problems/1week/Top-K-Frequent-Elements.py
@@ -0,0 +1,34 @@
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        d = dict()
+        
+        for i in nums:
+            d[i] = d.get(i, 0) + 1
+        
+        counts = [(value, key) for key, value in d.items()]
+        heap = []
+        for value, key in counts:
+            heapq.heappush(heap, (-value, key))
+                
+        while k > 0:
+            res.append(heapq.heappop(heap)[1])
+            k -= 1
+        return res
+        
+        
+        
+```
+# heapq 모듈 사용
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        d = dict()
+        
+        for i in nums:
+            d[i] = d.get(i, 0) + 1
+        
+        return heapq.nlargest(k, d, d.get)
+```
