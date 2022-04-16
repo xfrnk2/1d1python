756e64f230ac065ef14486bf3c1e7c76915fbcc8
xfrnk2 <xfrnk2@gmail.com>
2022-04-17 01:58:50 +0900

    feat: 2022-04-17

---
+++ 60-LeetCode-Problems/1week/Kth-Largest-Element-in-a-Stream.py
@@ -0,0 +1,18 @@
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.nums = []
+        for num in nums:
+            heapq.heappush(self.nums, num)
+        while len(self.nums) > k:
+            heapq.heappop(self.nums)
+
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.nums, val)
+        if len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        return self.nums[0]
