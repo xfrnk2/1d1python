2708db22fca2b1322c62f9a7bcf5b7021c814cc3
xfrnk2 <xfrnk2@gmail.com>
2022-04-17 01:59:21 +0900

    feat: 2022-04-17

---
+++ 60-LeetCode-Problems/1week/Valid-Parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        d = {
+            ')':'(',
+            '}':'{',
+            ']':'['
+        }
+        t = []
+        for c in s:
+            if c in '({[':
+                t.append(c)
+            elif c in ')}]':
+                if not t:
+                    return False
+                o = t.pop()
+                if d[c] != o:
+                    return False
+        return len(t) == 0
