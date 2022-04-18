b4723ec1a13723b746bac318cba3222e12c90b7c
xfrnk2 <xfrnk2@gmail.com>
2022-04-18 22:32:12 +0900

    feat: 2022-04-18

---
+++ 60-LeetCode-Problems/2week/Capacity-To-Ship-Packages-Within-D-Days.py
@@ -0,0 +1,26 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        
+        lo, hi = max(weights), sum(weights)
+        
+        while lo < hi:
+            
+            res = 0
+            mid = temp = (lo + hi)//2
+            
+            cnt = 0
+            level = mid
+            for w in weights:
+                
+                if level - w < 0:
+                    cnt += 1
+                    level = mid - w
+                else:
+                    level -= w
+            
+            if cnt < days:
+                hi = mid
+            else:
+                lo = mid + 1
+            
+        return lo
