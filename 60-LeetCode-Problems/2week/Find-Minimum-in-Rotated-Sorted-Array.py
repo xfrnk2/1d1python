c2e7a0ac8b8f2076dea22e4c1ceae3661a2acdec
xfrnk2 <xfrnk2@gmail.com>
2022-04-20 19:09:56 +0900

    feat: 2022-04-20

---
+++ 60-LeetCode-Problems/2week/Find-Minimum-in-Rotated-Sorted-Array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        lo, hi = 0, len(nums) - 1
+        cnt = 0
+        while lo < hi:
+            if nums[lo] < nums[hi]:
+                return nums[lo]
+            mid = (lo + hi)//2
+            
+            if nums[lo] <= nums[mid]:
+                lo = mid + 1
+            else:
+                hi = mid
+
+        return nums[lo]
+    
