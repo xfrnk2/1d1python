b554e1e62a8c40b0c9590694a49f619f86e24468
xfrnk2 <xfrnk2@gmail.com>
2022-05-03 23:55:03 +0900

    feat: 2022-05-02

---
+++ 60-LeetCode-Problems/3week/maximum-subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        d = [0] * len(nums)
+        
+        d[0] = nums[0] 
+        for i in range(1, len(nums)):
+            total = nums[i] + d[i-1]
+            if total > nums[i]:
+                d[i] = total
+            else:
+                d[i] = nums[i]
+        return max(d)
