fa28ba6cbd3499af2ac71a30215da3500c5e589f
xfrnk2 <xfrnk2@gmail.com>
2022-05-03 23:54:37 +0900

    feat: 2022-05-02

---
+++ 60-LeetCode-Problems/3week/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) <= 1:
+            return 0
+        
+        t = [0] * len(prices) + [0]
+        t[0] = prices[0]
+        m = t[0]
+        
+        for i in range(len(prices)):
+            m = min(m, prices[i])
+            if m < prices[i]:
+                t[i] = prices[i] - m
+        
+        print(t)
+
+        
+        return max(t[1:])
