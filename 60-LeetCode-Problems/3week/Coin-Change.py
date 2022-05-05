2056cb90f2c540307103c7bf59c815ef1dabb0b1
xfrnk2 <xfrnk2@gmail.com>
2022-05-05 11:22:09 +0900

    feat: 2022-05-03

---
+++ 60-LeetCode-Problems/3week/Coin-Change.py
@@ -0,0 +1,25 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        
+        coins = sorted(coins)
+        inf =  float('inf')
+        
+        d = [[0] * (amount + 1) for _ in range(len(coins) + 1)]
+        for j in range(1, amount+1):
+            d[0][j] = inf
+        
+        for i in range(1, len(coins) + 1):
+            for j in range(1, amount+1):
+                if coins[i-1] <= j:
+                    d[i][j] = min(d[i][j - coins[i-1]] + 1, d[i-1][j])
+                else:
+                    d[i][j] = d[i-1][j]
+
+        
+        res = d[-1][-1]
+        if res == inf:
+            return -1
+        return res
+
+        
