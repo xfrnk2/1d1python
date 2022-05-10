c9faa0e52f42be8996d98e3ab5486cf9e89d7877
xfrnk2 <xfrnk2@gmail.com>
2022-05-11 01:21:23 +0900

    feat:2022-05-10

---
+++ 60-LeetCode-Problems/4week/Pow(x,n).py
@@ -0,0 +1,24 @@
+class Solution:
+    
+    def calc(self, m, n):
+        if n == 0:
+            return 1
+        res = self.calc(m * m, n//2)
+        if n % 2 == 0:
+            return res
+        return res * m
+
+    
+    def myPow(self, x: float, n: int) -> float:
+        
+        negative = True
+        if n > 0:
+            negative = False
+            
+        if negative:
+            n *= -1
+            
+        res = self.calc(x, n)
+        if negative:
+            res = 1 // res
+        return res
