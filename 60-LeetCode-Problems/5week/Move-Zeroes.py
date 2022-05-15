fe82b815a24d2a28ffe54466334dfe864a2f74b5
xfrnk2 <xfrnk2@gmail.com>
2022-05-16 00:37:29 +0900

    feat: 2022-05-16

---
+++ 60-LeetCode-Problems/5week/Move-Zeroes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        idx = 0
+        zero = 0
+        
+        while idx < len(nums):
+            if nums[idx] == 0:
+                idx += 1
+                continue
+            nums[idx], nums[zero] = nums[zero], nums[idx]
+            zero += 1
+            idx += 1
+            
+            
