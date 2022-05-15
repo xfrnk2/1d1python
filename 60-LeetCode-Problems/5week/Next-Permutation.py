56c4fc6d873a58eda3dc4d69c74eafbb03178a26
xfrnk2 <xfrnk2@gmail.com>
2022-05-16 00:37:32 +0900

    feat: 2022-05-16

---
+++ 60-LeetCode-Problems/5week/Next-Permutation.py
@@ -0,0 +1,33 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+       
+        if len(nums) <= 1:
+            return nums
+        
+        
+        isMP = False
+        suffix = len(nums)-1
+        for i in range(len(nums)-1,  0, -1):
+            if nums[i-1] >= nums[i]:
+                if i == 1:
+                    isMP = True
+                continue                
+            suffix = i
+            break
+        
+        if isMP:
+            nums.reverse()
+            return
+        
+        p = len(nums)-1
+        while nums[suffix-1] >= nums[p]:
+            p -=1
+        
+        nums[p], nums[suffix-1] = nums[suffix-1], nums[p]
+        nums[suffix:] = nums[len(nums)-1:suffix-1:-1]
+
+                   
+   
