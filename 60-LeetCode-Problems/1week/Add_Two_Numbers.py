0e04478ee8541ef29eab65b21e63ebf459792689
xfrnk2 <xfrnk2@gmail.com>
2022-05-22 16:29:53 +0900

    build: create .gitignore

---
+++ 60-LeetCode-Problems/1week/Add_Two_Numbers.py
@@ -0,0 +1,40 @@
+"""
+leetcode problem solving
+"""
+
+class ListNode:
+    """
+    Definition for singly-linked list.
+    """
+    def __init__(self, val=0):
+        self.val = val
+        self.next = None
+
+
+class Solution:
+    """
+    solution
+    """
+    def add_two_numbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        n1, n2 = 0, 0
+
+        square_l1 = square_l2 = 0
+
+        while l1:
+            n1 += l1.val * 10 ** square_l1
+            square_l1 += 1
+            l1 = l1.next
+        while l2:
+            n2 += l2.val * 10 ** square_l2
+            square_l2 += 1
+            l2 = l2.next
+
+        res = list(str(n1 + n2))
+
+        last_node = ListNode(res[0], None)
+        for i in range(1, len(res)):
+            current_node = ListNode(res[i], last_node)
+            last_node = current_node
+
+        return last_node
