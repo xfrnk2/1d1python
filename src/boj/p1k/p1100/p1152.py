5ebfeceb3fca1eabd58aca458edc96997bdeb130
xfrnk2 <xfrnk2@gmail.com>
2022-05-28 23:25:28 +0900

    solve: 1152 단어의 갯수

---
+++ src/boj/p1k/p1100/p1152.py
@@ -0,0 +1,2 @@
+def number_of_words(sentence) -> None:
+    print(len(sentence.split()))
---
+++ tests/boj/p1k/p1100/p1152_test.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from typing import Sequence, Tuple
+import pytest
+
+from src.boj.p1k.p1100.p1152 import number_of_words
+
+cases: Sequence[Tuple[str, int]] = [
+("The Curious Case of Benjamin Button", 6),
+(" The first character is a blank", 6),
+("The last character is a blank ", 6)
+]
+
+@pytest.mark.parametrize("sentence, expected", cases)
+def test_number_of_words(sentence: str, expected: int, capsys):  # noqa
+    number_of_words(sentence)
+    captured = capsys.readouterr()
+    assert captured.out == f'{expected}\n'
