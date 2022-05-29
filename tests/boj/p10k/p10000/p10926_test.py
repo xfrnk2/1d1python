c602520521b256e1f9ac046bd29f666019cbf478
xfrnk2 <xfrnk2@gmail.com>
2022-05-29 11:32:03 +0900

    refactor: indentation

--- tests/boj/p10k/p10000/p10926_test.py
+++ tests/boj/p10k/p10000/p10926_test.py
@@ -3,9 +3,10 @@ from typing import Sequence, Tuple
 import pytest
 
 cases: Sequence[Tuple[str, str]] = [
-("joonas", "joonas??!"),
-("baekjoon", "baekjoon??!"),
+    ("joonas", "joonas??!"),
+    ("baekjoon", "baekjoon??!"),
 ]
+
 
 @pytest.mark.parametrize("user, expected", cases)
 def test_number_of_words(user: str, expected: str, capsys):  # noqa
