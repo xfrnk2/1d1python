963be7489bf6b0d1b400dea2e7461fe759da2876
xfrnk2 <xfrnk2@gmail.com>
2022-05-29 10:50:15 +0900

    solve: 18108 1998년생인 내가 태국에서는 2541년생?!

---
+++ src/boj/p18k/p18000/p18108.py
@@ -0,0 +1,2 @@
+def be_to_ad(be) -> None:
+    print(int(be) - 543)
---
+++ tests/boj/p18k/p18000/p18108_test.py
@@ -0,0 +1,13 @@
+from src.boj.p18k.p18000.p18108 import be_to_ad
+from typing import Sequence, Tuple
+import pytest
+
+cases: Sequence[Tuple[str, int]] = [
+("2541", 1998)
+]
+
+@pytest.mark.parametrize("be, expected", cases)
+def test_be_to_ad(be: str, expected: int, capsys):  # noqa
+    be_to_ad(be)
+    captured = capsys.readouterr()
+    assert captured.out == f'{expected}\n'
