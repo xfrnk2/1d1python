ea1405d55f5cbe4c3545f8d0941728cffd7889a9
xfrnk2 <xfrnk2@gmail.com>
2022-05-30 19:11:18 +0900

    test: 13305 주유소

---
+++ tests/boj/p13k/p13300/p13305_test.py
@@ -0,0 +1,15 @@
+from src.boj.p13k.p13300.p13305 import gas_station
+from typing import Sequence, Tuple
+import pytest
+
+cases: Sequence[Tuple[str, int]] = [
+("4", "2 3 1", "5 2 4 1" 18),
+("4", "3 3 4", "1 1 1 1" 10)
+]
+
+
+@pytest.mark.parametrize("n, distances, stations, expected", cases)
+def test_be_to_ad(n: str, distances: str, stations: str, expected: int, capsys):  # noqa
+    choose_quadrant(n, distances, stations)
+    captured = capsys.readouterr()
+    assert captured.out == f'{expected}\n'
