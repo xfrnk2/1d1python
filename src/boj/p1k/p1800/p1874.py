c2b6a61d3e04ee7a9d053bc9c3f8f12498884a71
xfrnk2 <xfrnk2@gmail.com>
2022-06-02 21:59:35 +0900

    refactor: flake8

--- src/boj/p1k/p1800/p1874.py
+++ src/boj/p1k/p1800/p1874.py
@@ -1,12 +1,12 @@
 def stack_sequence(input_value_n: str, input_value_numbers: str) -> None:
     n = int(input_value_n)
     numbers = list(map(int, input_value_numbers.split()))
-    
+
     cnt = 0
     res = []
     stack = []
     isValid = True
-    
+
     for i in range(n):
         cur = numbers.pop(0)
         while cnt < cur:
@@ -19,10 +19,8 @@ def stack_sequence(input_value_n: str, input_value_numbers: str) -> None:
             continue
         isValid = False
         break
-    
+
     if isValid:
         print("\n".join(res))
         return
     print("NO")
-        
-            
