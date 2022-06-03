479ea00359fe1e7f2d0139a80d97d7b3069dc6c0
xfrnk2 <xfrnk2@gmail.com>
2022-06-03 22:32:13 +0900

    refactor: 1874 스택수열

--- src/boj/p1k/p1800/p1874.py
+++ src/boj/p1k/p1800/p1874.py
@@ -1,26 +1,26 @@
 def stack_sequence(input_value_n: str, input_value_numbers: str) -> None:
-    n = int(input_value_n)
-    numbers = list(map(int, input_value_numbers.split()))
+    N = int(input_value_n)
+    given_numbers = list(map(int, input_value_numbers.split()))
 
-    cnt = 0
-    res = []
+    current_idx = 0
+    answer = []
     stack = []
-    isValid = True
+    is_avaiable = True
 
-    for i in range(n):
-        cur = numbers.pop(0)
-        while cnt < cur:
-            cnt += 1
-            res.append("+")
-            stack.append(cnt)
-        if stack[-1] == cur:
+    for _ in range(N):
+        current_number = given_numbers.pop(0)
+        while current_idx < current_number:
+            current_idx += 1
+            answer.append("+")
+            stack.append(current_idx)
+        if stack[-1] == current_number:
             stack.pop()
-            res.append("-")
+            answer.append("-")
             continue
-        isValid = False
+        is_avaiable = False
         break
 
-    if isValid:
-        print("\n".join(res))
+    if is_avaiable:
+        print("\n".join(answer))
         return
     print("NO")
