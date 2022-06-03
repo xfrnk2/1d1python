20c05d86e75f3ffd9aa919467f4e2187dfea6a1f
xfrnk2 <xfrnk2@gmail.com>
2022-06-03 22:27:02 +0900

    refactor: 17298 오큰수

--- src/boj/p17k/p17200/p17298.py
+++ src/boj/p17k/p17200/p17298.py
@@ -2,14 +2,14 @@ from typing import List
 
 
 def right_big_number(input_value_n: str, input_value_numbers: str) -> None:
-    n = int(input_value_n)
-    numbers = list(map(int, input_value_numbers.split(' ')))
-    right_big_numbers = [-1 for _ in range(n)]
+    N = int(input_value_n)
+    given_numbers = list(map(int, input_value_numbers.split(' ')))
     stack: List[List[int]] = []
+    answer = [-1 for _ in range(N)]
 
-    for i in range(n):
-        while stack and stack[-1][0] < numbers[i]:
-            val, idx = stack.pop()
-            right_big_numbers[idx] = numbers[i]
-        stack.append([numbers[i], i])
-    print(*right_big_numbers)
+    for i in range(N):
+        while stack and stack[-1][1] < given_numbers[i]:
+            idx, number = stack.pop()
+            answer[idx] = given_numbers[i]
+        stack.append([i, given_numbers[i]])
+    print(*answer)
