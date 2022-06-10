1183aaad0fcc7c50cc807c39d735efda3548bc6a
xfrnk2 <xfrnk2@gmail.com>
2022-06-10 12:55:26 +0900

    solve: 18100 마인크래프트

--- src/boj/p18k/p18100/p18111.py
+++ src/boj/p18k/p18100/p18111.py
@@ -1,45 +1,25 @@
-def minecraft(info: str, row1: str, row2: str, row3: str) -> None:
-    N, M, B = map(int, info.split())
-    field = list(map(int, row1.split())) + list(map(int, row2.split())) + list(map(int, row3.split()))
-    
-    up_required_amount = 0
-    down_required_amount = 0
-    
-    min_value, max_value = min(field), max(field)
-    
-    up_left_block = B
-    down_left_block = B
-    up_time = 0
-    down_time = 0
-    compare = []
-    
-    if B == 0:
-        idx = 0
-        while idx < len(field):
-            amount = field[idx] - min_value
-            down_left_block += amount
-            down_time += amount * 2
-            idx += 1
-        print(down_time, min_value)
-        return
+from typing import List
 
-    idx = 0
-    # 범위를 벗어나지 않고 남은 블럭이 있을 경우 계속한다
-    while up_left_block > 0 and idx < len(field):
-        # 최댓값에서 현재 위치 값의 차이를 계산하고, 시간을 계산하여 넣는다.
-        amount = max_value - field[idx]
-        up_left_block += amount
-        up_time += amount
-        idx += 1
-    compare.append((up_time, max_value))
-    
-    idx = 0
-    while down_left_block > 0 and idx < len(field):
-        amount = field[idx] - min_value
-        down_left_block += amount
-        down_time += amount * 2
-        idx += 1
-        
-    compare.append((down_time, min_value))
 
-    print(*min(compare, key= lambda x: (x[0], x[1])))
+def minecraft(_info: str, _field: List[str]) -> None:
+    N, M, B = map(int, _info.split())
+    field = []
+    for f in _field:
+        field.extend(list(map(int, f.split())))
+    times = []
+
+    inclusive, exclusive = min(field), max(field) + 1
+    for p in range(inclusive, exclusive):
+        time: int = 0
+        blocks: int = B
+        for k in range(N * M):
+            cur: int = field[k]
+            if cur < p:
+                time += p - cur
+                blocks -= p - cur
+            if cur > p:
+                time += (cur - p) * 2
+                blocks += cur - p
+        if blocks >= 0:
+            times.append((time, p))
+    print(*min(times, key=lambda t: (t[0], -t[1])))
--- tests/boj/p18k/p18100/p18111_test.py
+++ tests/boj/p18k/p18100/p18111_test.py
@@ -1,17 +1,19 @@
 from src.boj.p18k.p18100.p18111 import minecraft
-from typing import Sequence, Tuple
+from typing import Sequence, Tuple, List
 import pytest
 
-cases: Sequence[Tuple[str, int]] = [
-("3 4 99", "0 0 0 0", "0 0 0 0", "0 0 0 1", "2 0"),
-("3 4 1", "64 64 64 64", "64 64 64 64", "64 64 64 63", "1 64"),
-("3 4 0", "64 64 64 64", "64 64 64 64", "64 64 64 63", "22 63")
+cases: Sequence[Tuple[str, List[str], str]] = [
+    ("3 4 99", ["0 0 0 0", "0 0 0 0", "0 0 0 1"], "2 0"),
+    ("3 4 1", ["64 64 64 64", "64 64 64 64", "64 64 64 63"], "1 64"),
+    ("3 4 0", ["64 64 64 64", "64 64 64 64", "64 64 64 63"], "22 63"),
+    ("3 4 11", ["29 51 54 44", "22 44 32 62", "25 38 16 2"], "250 35"),
+    ("4 4 36", ["15 43 61 21", "19 33 31 55", "48 63 1 30", "31 28 3 8"], "355 32")
 
 ]
 
 
-@pytest.mark.parametrize("info, row1, row2, row3, expected", cases)
-def test_minecraft(info: str, row1: str, row2: str, row3: str, expected: str, capsys):  # noqa
-    minecraft(info, row1, row2, row3)
+@pytest.mark.parametrize("info, field, expected", cases)
+def test_minecraft(info: str, field: List[str], expected: str, capsys):  # noqa
+    minecraft(info, field)
     captured = capsys.readouterr()
     assert captured.out == f'{expected}\n'
