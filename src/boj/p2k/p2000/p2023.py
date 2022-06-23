bef53d4e020b69d3178978dca3f146247205d205
xfrnk2 <xfrnk2@gmail.com>
2022-06-23 15:17:10 +0900

    refactor: 2023 신기한소수

--- src/boj/p2k/p2000/p2023.py
+++ src/boj/p2k/p2000/p2023.py
@@ -1,11 +1,10 @@
+import math
 from typing import List
 
 
-def is_decimal(a):
-    if (a < 2):
-        return False
-    for i in range(2, a):
-        if (a % i == 0):
+def is_decimal(n):
+    for i in range(2, int(math.sqrt(n)) + 1):  # n의 제곱근을 정수화 시켜준 후 + 1
+        if n % i == 0:
             return False
     return True
 
@@ -15,7 +14,6 @@ def solve(n: int, l: int, x: int, result: List[int]) -> None:
         result.append(x)
         return
     for i in range(1, 10):
-
         nx = x * 10 + i
 
         if is_decimal(nx):
@@ -24,6 +22,7 @@ def solve(n: int, l: int, x: int, result: List[int]) -> None:
 
 def amazing_decimal(n: int) -> List[int]:
     answer = []
+
     for i in [2, 3, 5, 7]:
         solve(n, 1, i, answer)
     return answer
