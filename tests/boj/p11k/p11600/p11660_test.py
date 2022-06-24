b42dc6df05eaf7f658739a4f226a8a1842807ddc
xfrnk2 <xfrnk2@gmail.com>
2022-06-24 20:19:09 +0900

    fix: mypy

--- tests/boj/p11k/p11600/p11660_test.py
+++ tests/boj/p11k/p11600/p11660_test.py
@@ -2,7 +2,7 @@ from src.boj.p11k.p11600.p11660 import prefix_sum
 from typing import List, Sequence, Tuple
 import pytest
 
-cases: Sequence[Tuple[int, int, List[int], List[int], List[int]]] = [
+cases: Sequence[Tuple[int, int, List[List[int]], List[List[int]], List[int]]] = [
     (4, 3, [[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7]],
     [[2, 2, 3, 4], [3, 4, 3, 4], [1, 1, 4, 4]], [27, 6, 64])
 ]
