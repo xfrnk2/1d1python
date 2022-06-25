f3b05935e304374a2bbb76518187760bd71e8bf8
xfrnk2 <xfrnk2@gmail.com>
2022-06-25 17:26:00 +0900

    refactor: coverage

--- src/boj/p14k/p14600/p14681.py
+++ src/boj/p14k/p14600/p14681.py
@@ -1,12 +1,10 @@
 def choose_quadrant(input_value_x: str, input_value_y: str) -> None:
     x, y = int(input_value_x), int(input_value_y)
+    answer = 1
     if x < 0 and y < 0:
-        print(3)
-        return
-    if x < 0 and y > 0:
-        print(2)
-        return
-    if x > 0 and y < 0:
-        print(4)
-        return
-    print(1)
+        answer = 3
+    elif x < 0 and y > 0:
+        answer = 2
+    elif x > 0 and y < 0:
+        answer = 4
+    print(answer)
--- src/boj/p9k/p9400/p9498.py
+++ src/boj/p9k/p9400/p9498.py
@@ -1,15 +1,12 @@
 def exam_score(input_value: str):
     score = int(input_value)
+    answer = "F"
     if 90 <= score <= 100:
-        print("A")
-        return
-    if 80 <= score < 90:
-        print("B")
-        return
-    if 70 <= score < 80:
-        print("C")
-        return
-    if 60 <= score < 70:
-        print("D")
-        return
-    print("F")
+        answer = "A"
+    elif 80 <= score < 90:
+        answer = "B"
+    elif 70 <= score < 80:
+        answer = "C"
+    elif 60 <= score < 70:
+        answer = "D"
+    print(answer)
