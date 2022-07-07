634f165ba9ac002fd0be7eca0748d83631f20d6d
xfrnk2 <xfrnk2@gmail.com>
2022-07-08 06:34:35 +0900

    docs: add annotation in 11724 연결 요소의 개수

--- src/boj/p11k/p11700/p11724.py
+++ src/boj/p11k/p11700/p11724.py
@@ -11,6 +11,11 @@ def visit(vertex_data: dict, visited: List[int], vertex1: int) -> None:
 
 
 def connected_elements_number(n: int, m: int, edges: List[List[int]]) -> None:
+    """
+    1. 모든 입력값을 각 숫자별 배열을 만들어 저장한다. (예: 1과 대응하는 1과 연결된 리스트)
+    2. 1~ N까지 순회한다.
+    3. 방문한 목록에 추가해가며 모든 연결된 정점을 방문한다.
+    """
     vertex_data = defaultdict(list)
     visited = [0 for _ in range(n + 1)]  # 방문시 1
     count = 0
