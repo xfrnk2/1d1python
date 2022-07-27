2440291c25f80f954beddfa540a54abdf92a3b4a
xfrnk2 <xfrnk2@gmail.com>
2022-07-28 00:12:58 +0900

    solve: 16236 아기상어

--- src/boj/p16k/p16200/p16236.py
+++ src/boj/p16k/p16200/p16236.py
@@ -1,46 +1,81 @@
-from typing import List, Tuple
+from collections import deque
+from typing import Any, List, Tuple
+
+dy = [0, 0, -1, 1]
+dx = [-1, 1, 0, 0]
 
 
-def check_is_movable(field: List[List[int]], x: int, y: int) -> boolean:
-    return True
-
-
-def get_eatable_fish(field: List[List[int]]):
-    result: List[Tuple[int, int]]
+def get_eatable_fish_coordinates(size: int, field: List[List[int]], distance: List[List[Any]]) -> List[
+    Tuple[int, int, int]]:
+    n = len(field)
     result = []
-    
     for i in range(n):
-            for j in range(n):
-                if 2 < field[i][j]:
-                    result.append((i, j))
+        for j in range(n):
+            if 0 < field[i][j] < size:
+                result.append((i, j, distance[i][j]))
     return result
 
-def baby_shark(n: int, _field: List[List[int]]) -> int:
-    field = []
-    for f in _field:
-        field.append(f)
 
-    start = None
-    size = 2
-    move_count = 0
+def check_is_valid_range(n: int, x: int, y: int) -> bool:
+    return 0 <= x < n and 0 <= y < n
 
+
+def get_distance_matrix(size: int, start_x: int, start_y: int, field: List[List[int]]) -> List[List[float]]:
+    n = len(field)
+
+    distance = [[float('inf')] * n for _ in range(n)]
+    q = deque([(start_x, start_y)])
+    distance[start_x][start_y] = 0
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if not check_is_valid_range(n, nx, ny):
+                continue
+            if size < field[nx][ny]:
+                continue
+
+            if distance[nx][ny] > distance[x][y] + 1:
+                distance[nx][ny] = distance[x][y] + 1
+                q.append((nx, ny))
+
+    return distance
+
+
+def find_shark(n: int, field: List[List[int]]) -> Tuple[int, int]:
     for i in range(n):
         for j in range(n):
             if field[i][j] == 9:
-                start = (i, j)
-    print(start)
-
-    dx = [0, 0, -1, 1]
-    dy = [-1, 1, 0, 0]
+                return i, j
+    return 0, 0
 
 
-    is_continuable = True
-    while is_continuable:
-        eatable_fish = get_eatable_fish(field)
-        if not eatable_fish:
-            is_continuable = False
+def baby_shark(n: int, field: List[List[int]]) -> int:
+    move_count = 0
+    start_x, start_y = find_shark(n, field)
+    size = 2
+    required_fish = size
+
+    field[start_x][start_y] = 0
+
+    while True:
+
+        distance = get_distance_matrix(size, start_x, start_y, field)
+        targets = get_eatable_fish_coordinates(size, field, distance)
+
+        if not targets:
             break
 
-        if len(eatable_fish) == 1:
-            
-        
+        targets.sort(key=lambda x: (x[2], x[0], x[1]))
+        start_x, start_y = targets[0][0], targets[0][1]
+        move_count += targets[0][2]
+        field[start_x][start_y] = 0
+        required_fish -= 1
+        if required_fish <= 0:
+            size += 1
+            required_fish = size
+ 
+    return move_count
