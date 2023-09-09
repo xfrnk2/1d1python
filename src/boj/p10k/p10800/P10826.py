91617f08a7d5f26d219f226df7c30e241a8424ce
xfrnk2 <34790699+xfrnk2@users.noreply.github.com>
2023-09-10 01:40:49 +0900

    fix: 10826 피보나치수 4

--- src/boj/p10k/p10800/P10826.py
+++ src/boj/p10k/p10800/P10826.py
@@ -1,10 +1,6 @@
-import sys
-input = sys.stdin.readline
-
-fibo = [0,1]
+fibo = [0, 1]
 n = int(input())
 
-for i in range(2, n+1):
-    fibo.append(fibo[i-2] + fibo[i-1])
-    
+for i in range(2, n + 1):
+    fibo.append(fibo[i - 2] + fibo[i - 1])
 print(fibo[n])
